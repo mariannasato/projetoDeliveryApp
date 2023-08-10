4a9968d32b0d32690aac6489d16f55e7470765d0
mariannasato <mariannasatto@gmail.com>
2023-08-10 19:01:50 -0300

    js implementation, unfinished

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # projetoDeliveryApp
 🛵 Mobile website for food delivery with four options on three categories. Introduction to javascript and practice around position/layouts.
+This project, currently on development, is being crucial for my learning on DOM manipulation, classlist, innerHTML and etc.
--- script.js
+++ script.js
@@ -1,7 +1,66 @@
-function selecionado(){
-    const option = document.querySelector(".opcao");
-    option.classList.toggle('selecionado');
-    
-    const check = document.querySelector(".check");
-    check.classList.toggle('hidden');
+let i = 0;
+
+function selecionarPrato(opcaoSelecionada){
+    const botaoClicado = document.querySelector(".prato.selecionado")
+    if (botaoClicado !== null){
+        botaoClicado.classList.remove("selecionado");
+        i = i - 1
+    }
+
+    opcaoSelecionada.classList.add("selecionado");
+
+    i = i + 1
+    console.log(i)
+
+    if (i == 3){
+        const finalizarPedido = document.querySelector("footer button");
+        finalizarPedido.innerHTML = "Fechar Pedido"
+        finalizarPedido.classList.add("botaoSelecionado")
+    }
+}
+
+function selecionarBebida(opcaoSelecionada){
+    const botaoClicado = document.querySelector(".bebida.selecionado")
+    if (botaoClicado !== null){
+        botaoClicado.classList.remove("selecionado");
+        i = i - 1
+    }
+
+    opcaoSelecionada.classList.add("selecionado");
+
+    i = i + 1
+    console.log(i)
+
+    if (i == 3){
+        const finalizarPedido = document.querySelector("footer button");
+        finalizarPedido.innerHTML = "Fechar Pedido"
+        finalizarPedido.classList.add("botaoSelecionado")
+    }
+
+    }
+
+
+
+function selecionarSobremesa(opcaoSelecionada){
+    const botaoClicado = document.querySelector(".sobremesa.selecionado")
+    if (botaoClicado !== null){
+        botaoClicado.classList.remove("selecionado");
+        i = i - 1
+    }
+
+    opcaoSelecionada.classList.add("selecionado");
+
+    i = i + 1
+    console.log(i)
+
+    if (i == 3){
+        const finalizarPedido = document.querySelector("footer button");
+        finalizarPedido.innerHTML = "Fechar Pedido"
+        finalizarPedido.classList.add("botaoSelecionado")
+    }
+}
+
+function confirmarPedido() {
+    const telaConfirmar = document.querySelector(".confirmarPedido")
+    telaConfirmar.classList.remove("hidden");
 }
