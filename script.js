6fb376987c1921d0c2615b72c9722e3f78b3d47d
mariannasato <mariannasatto@gmail.com>
2023-08-14 18:33:07 -0300

    confirm screen working

--- script.js
+++ script.js
@@ -6,11 +6,16 @@ function selecionarPrato(opcaoSelecionada){
         botaoClicado.classList.remove("selecionado");
         i = i - 1
     }
+    
 
     opcaoSelecionada.classList.add("selecionado");
 
+    const checkGeral = document.querySelector(".prato img.check")
+    checkGeral.classList.add("hidden")
+    const checkmark = document.querySelector(".prato.selecionado img.check")
+    checkmark.classList.remove("hidden")
+
     i = i + 1
-    console.log(i)
 
     if (i == 3){
         const finalizarPedido = document.querySelector("footer button");
@@ -29,7 +34,6 @@ function selecionarBebida(opcaoSelecionada){
     opcaoSelecionada.classList.add("selecionado");
 
     i = i + 1
-    console.log(i)
 
     if (i == 3){
         const finalizarPedido = document.querySelector("footer button");
@@ -51,7 +55,6 @@ function selecionarSobremesa(opcaoSelecionada){
     opcaoSelecionada.classList.add("selecionado");
 
     i = i + 1
-    console.log(i)
 
     if (i == 3){
         const finalizarPedido = document.querySelector("footer button");
@@ -60,13 +63,41 @@ function selecionarSobremesa(opcaoSelecionada){
     }
 }
 
-function confirmarPedido(pratoTitulo) {
+
+function confirmarPedido() {
     if (i == 3) {
     const telaConfirmar = document.querySelector(".confirmarPedido")
     telaConfirmar.classList.remove("hidden");
 
-    const tituloPrato = document.querySelector(".itemPedido p")
-    tituloPrato.innerHTML = pratoTitulo
+    const pratoTitulo = document.querySelector(".prato.selecionado h1");
+    const confirmarPrato = document.querySelector(".itemPedido h1")
+    confirmarPrato.innerHTML = pratoTitulo.innerHTML;
+
+    const pratoPreco = document.querySelector(".prato.selecionado h2");
+    const confirmarPratoPreco = document.querySelector(".itemPedido p")
+    confirmarPratoPreco.innerHTML = pratoPreco.innerHTML;
+
+    const bebidaTitulo = document.querySelector(".bebida.selecionado h1");
+    const confirmarBebida = document.querySelector(".itemPedido.bebida h1");
+    confirmarBebida.innerHTML = bebidaTitulo.innerHTML;
+
+    const bebidaPreco = document.querySelector(".bebida.selecionado h2");
+    const confirmarBebidaPreco = document.querySelector(".itemPedido.bebida p")
+    confirmarBebidaPreco.innerHTML = bebidaPreco.innerHTML;
+
+    const sobremesaTitulo = document.querySelector(".sobremesa.selecionado h1");
+    const confirmarSobremesa = document.querySelector(".itemPedido.sobremesa h1");
+    confirmarSobremesa.innerHTML = sobremesaTitulo.innerHTML;
+
+    const sobremesaPreco = document.querySelector(".sobremesa.selecionado h2");
+    const confirmarSobremesaPreco = document.querySelector(".itemPedido.sobremesa p")
+    confirmarSobremesaPreco.innerHTML = sobremesaPreco.innerHTML;
+
+    const pedidoTotal = document.querySelector(".itemPedido.total p");
+    valorTotal =  Number((parseInt(pratoPreco.innerHTML.replace(/\D/g, "")))+(parseInt(bebidaPreco.innerHTML.replace(/\D/g, "")))+(parseInt(sobremesaPreco.innerHTML.replace(/\D/g, ""))))
+    pedidoTotal.innerHTML = "R$ "+ valorTotal
+
+    // const fundoCinza = document.querySelector("body").classList.toggle("fundoCinza");
     }
 }
 
